f98fd8af8b2db101ed5c01b055832beabb06e2b1
Jesse Zhang <sbjesse@gmail.com>
2015-03-03 10:24:05 -0800

    Fish config and fish prompt with Git
    Kinda Mac-specific for now

---
+++ config.fish
@@ -0,0 +1,9 @@
+set -x GIT_EDITOR "mvim -f"
+
+# until fish 2.2, we'll have to do this
+set PATH (brew --prefix)/bin $PATH
+
+complete -f -c git -n '__fish_git_using_command pull' -l ff-only -d 'Refuse to merge unless fast-forward possible'
+complete -f -c git -n '__fish_git_using_command pull' -l rebase -d 'Rebase instead of merge'
+complete -c git -n '__fish_git_using_command commit' -s v -l verbose -d 'Show diff in commit message template'
+complete -c git -n '__fish_git_using_command commit' -s a -l all -d 'Commit all changed files'
