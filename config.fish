660dac921b340f5bd7c06ccea0a68c30c2be287b
Jesse Zhang <sbjesse@gmail.com>
2015-04-02 14:24:44 -0700

    complete git log --decorate

--- config.fish
+++ config.fish
@@ -12,3 +12,4 @@ complete -f -c git -n '__fish_git_using_command pull' -l ff-only -d 'Refuse to m
 complete -f -c git -n '__fish_git_using_command pull' -l rebase -d 'Rebase instead of merge'
 complete -c git -n '__fish_git_using_command commit' -s v -l verbose -d 'Show diff in commit message template'
 complete -c git -n '__fish_git_using_command commit' -s a -l all -d 'Commit all changed files'
+complete -f -c git -n '__fish_git_using_command log' -l decorate -a 'short full no'
