71b1b7d066fd3b38fcd7d1caded99fcaada93f2e
Jesse Zhang <sbjesse@gmail.com>
2015-05-06 08:36:56 -0700

    Adds `git push force-with-lease`

--- config.fish
+++ config.fish
@@ -13,6 +13,7 @@ complete -f -c git -n '__fish_git_using_command pull' -l rebase -d 'Rebase inste
 complete -c git -n '__fish_git_using_command commit' -s v -l verbose -d 'Show diff in commit message template'
 complete -c git -n '__fish_git_using_command commit' -s a -l all -d 'Commit all changed files'
 complete -f -c git -n '__fish_git_using_command log' -l decorate -a 'short full no'
+complete -f -c git -n '__fish_git_using_command push' -l force-with-lease -d 'require old value of ref to be at this value'
 
 set -x GOPATH ~/.local/go
 set PATH $GOPATH/bin ~/.rbenv/shims $PATH
