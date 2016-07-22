52f925c52a45e6dd4c9e343b6dc494a5b1971f2c
Jesse Zhang <sbjesse@gmail.com>
2016-07-22 01:45:26 -0700

    Automatically detect `fly` commands
    Here's a sequence of user typing and completions:
    ```
    Jesses-MacBook-Air ~> fly -<TAB>
    Jesses-MacBook-Air ~> fly --target
    Jesses-MacBook-Air ~> fly --target <TAB>
    Jesses-MacBook-Air ~> fly --target gporca
    Jesses-MacBook-Air ~> fly --target gporca <TAB>
    Jesses-MacBook-Air ~> fly --target gporca
    abort-build  destroy-pipeline  login           rename-pipeline  unpause-job       workers
    builds       execute           pause-job       set-pipeline     unpause-pipeline
    checklist    get-pipeline      pause-pipeline  sync             volumes
    containers   hijack            pipelines       trigger-job      watch
    ```

--- completions/fly.fish
+++ completions/fly.fish
@@ -2,6 +2,11 @@ function __fish_fly_targets
 	sed -n 's/^  \([^[:space:]].*\):/\\1/p' ~/.flyrc
 end
 
+function __fish_fly_commands
+	fly --help ^| sed -n '/Available commands:/,$p' | sed -E -n 's/  ([-[:alpha:]]+).*/\1/p'
+end
+
 complete --exclusive --command fly --long-option target --arguments '(__fish_fly_targets)' --description "Concourse target name"
 complete --command fly --long-option help --description "Show this help message"
 complete --command fly --long-option version --description "Print the version of Fly and exit"
+complete --command fly --no-files --arguments "(__fish_fly_commands)"
