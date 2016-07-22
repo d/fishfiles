1daafd92c1dc4338d68001b2b147a99a4fe70f90
Jesse Zhang <sbjesse@gmail.com>
2016-07-22 08:40:39 -0700

    complete options for `fly hijack`

--- completions/fly.fish
+++ completions/fly.fish
@@ -12,7 +12,6 @@ function __fish_fly_targeted
 	if [ (count $cmd) -eq 1 ]
 		return 1
 	else
-		echo $cmd
 		for c in $cmd[2..-1]
 			if [ $c = "--target" ]
 				return 0
@@ -22,7 +21,40 @@ function __fish_fly_targeted
 	return 1
 end
 
+function __fish_fly_hijack
+	set --local cmd (commandline -opc)
+
+	if [ (count $cmd) -gt 1 ]
+		set --local skip_next 1
+		for c in $cmd[2..-1]
+			if [ $skip_next -eq 0 ]
+				set skip_next 1
+				continue
+			end
+			switch $c
+				case '--target'
+					set skip_next 0
+					continue
+				case 'hijack'
+					return 0
+				case '--*'
+					continue
+				case '*'
+					return 1
+			end
+		end
+	else
+		return 1
+	end
+end
+
 complete --exclusive --command fly --long-option target --arguments '(__fish_fly_targets)' --description "Concourse target name"
 complete --command fly --long-option help --description "Show this help message"
 complete --command fly --long-option version --description "Print the version of Fly and exit"
 complete --command fly --no-files --condition '__fish_fly_targeted' --arguments '(__fish_fly_commands)'
+
+complete --command fly --condition '__fish_fly_hijack' --long-option job --description 'Name of a job to hijack'
+complete --command fly --condition '__fish_fly_hijack' --long-option check --description "Name of a resource's checking container to hijack"
+complete --command fly --condition '__fish_fly_hijack' --long-option build --description 'Build number within the job, or global build ID'
+complete --command fly --condition '__fish_fly_hijack' --long-option step --description 'Name of step to hijack (e.g. build, unit, resource name)'
+complete --command fly --condition '__fish_fly_hijack' --long-option attempt --description 'Attempt number of step to hijack. Can be specified multiple times for nested retries'
