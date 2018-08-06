a29f7765d29b6de3f6290078c331e835bcc0c308
Jesse Zhang <sbjesse@gmail.com>
2018-08-05 21:48:32 -0700

    Remove git completions that are better in upstream
    Fish 2.7 already completes these flags better than we do.

--- config.fish
+++ config.fish
@@ -9,7 +9,5 @@ complete -f -c git -n '__fish_git_using_command pull' -l ff-only -d 'Refuse to m
 complete -f -c git -n '__fish_git_using_command pull' -l rebase -d 'Rebase instead of merge'
 complete -c git -n '__fish_git_using_command commit' -s v -l verbose -d 'Show diff in commit message template'
 complete -c git -n '__fish_git_using_command commit' -s a -l all -d 'Commit all changed files'
-complete -f -c git -n '__fish_git_using_command log' -l decorate -a 'short full no'
-complete -f -c git -n '__fish_git_using_command push' -l force-with-lease -d 'require old value of ref to be at this value'
 
 set PATH ~/.rbenv/shims $PATH
