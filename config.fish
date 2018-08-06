f79ed0d9f8eca6055c2c518d83e8f40dfb9c5795
Jesse Zhang <sbjesse@gmail.com>
2018-08-05 21:58:09 -0700

    Suppress warnings of non-existent rbenv shims dir

--- config.fish
+++ config.fish
@@ -5,4 +5,6 @@ complete -f -c git -n '__fish_git_using_command pull' -l rebase -d 'Rebase inste
 complete -c git -n '__fish_git_using_command commit' -s v -l verbose -d 'Show diff in commit message template'
 complete -c git -n '__fish_git_using_command commit' -s a -l all -d 'Commit all changed files'
 
-set PATH ~/.rbenv/shims $PATH
+if test -d ~/.rbenv/shims;
+	set PATH ~/.rbenv/shims $PATH
+end
